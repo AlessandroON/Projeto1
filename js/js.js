d33e64a1a23fd1488fbbe67e4d0136086b24c245
AlessandroON <alessandrooliveira645.ao@gmail.com>
2026-02-21 19:06:20 -0300

    att

--- js/js.js
+++ js/js.js
@@ -5,3 +5,17 @@ cards.forEach(card => {
         card.classList.toggle("ativo");
     });
 });
+
+const btnVermais = document.getElementById('vermais');
+const section1 = document.getElementById('section1');
+
+btnVermais.addEventListener('click', () => {
+    section1.classList.toggle('hidden');
+
+    if (section1.classList.contains('hidden')) {
+        btnVermais.textContent = 'Ver mais';
+    } else {
+        btnVermais.textContent = 'Ver menos';
+    }
+});
+
